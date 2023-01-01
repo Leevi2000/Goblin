729962d79f7760c606cafe77fc5040d7418a057b
Leevi Eskola <leevieskola@gmail.com>
2023-01-01 13:12:50 +0200

    Created unity project

---
+++ Assets/Database/DB_Backend.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mono.Data.Sqlite;
+
+namespace Database.Backend
+{
+   public class DB_Backend : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
